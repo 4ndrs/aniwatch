97b3bfe5470ca166248018b10517485e3676352b
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2025-08-19 11:51:47 +0200

    Consider empty relations state

--- app/anime/[id]/page.tsx
+++ app/anime/[id]/page.tsx
@@ -11,15 +11,9 @@ type Props = {
 
 const Overview = ({ params }: Props) => (
   <div>
-    <section className="space-y-2.5">
-      <h2 className="text-sm leading-4 font-medium">Relations</h2>
-
-      <div className="flex flex-wrap gap-5">
-        <Suspense fallback={<div>Loading relations...</div>}>
-          <Relations params={params} />
-        </Suspense>
-      </div>
-    </section>
+    <Suspense fallback={<div>Loading relations...</div>}>
+      <Relations params={params} />
+    </Suspense>
   </div>
 );
 
@@ -27,9 +21,21 @@ const Relations = async ({ params }: Props) => {
   const id = Number((await params).id);
   const relations = (await loadAnime(id))?.anime?.relations;
 
-  return relations?.edges?.map((relation) => (
-    <RelationCard key={relation?.id} relation={relation} />
-  ));
+  if (!relations?.edges?.length) {
+    return;
+  }
+
+  return (
+    <section className="space-y-2.5">
+      <h2 className="text-sm leading-4 font-medium">Relations</h2>
+
+      <div className="flex flex-wrap gap-5">
+        {relations?.edges?.map((relation) => (
+          <RelationCard key={relation?.id} relation={relation} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Overview;
